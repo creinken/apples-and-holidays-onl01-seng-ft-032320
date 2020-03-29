63150f2c304032d4ddf676640a30041eb327b282
Christopher Reinken <christopherreinken@gmail.com>
2020-03-29 08:21:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,6 +81,12 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_supply_string << "  #{holiday_string_array.join(" ")}:"
       if supply.length == 1
         holiday_supply_string << "#{supply.to_s.capitalize!}"}
+      else
+        (supply.length - 2).times do |x|
+          holiday_supply_string << "#{supply[x-1].capitalize!}, "
+        end
+        holiday_supply_string << "#{supply[-1].capitalize!}"
+      end
       puts holiday_supply_string
     end
   end
